8be9fc19c289e4799280dc72dcc548a6dd00e0b9
ITRACCON <73070769+ITRACCON@users.noreply.github.com>
2021-03-12 18:59:26 +0300

    Update ases.js

--- ases.js
+++ ases.js
@@ -192,7 +192,7 @@ this.getTeamsRaiting = async function (callback) {
 };
 
 
-this.getMatchAnalitic = async  function (callback, urlMatch) {
+this.getMatchAnalitic = async  function (urlMatch) {
     const getHTML = async (url) => {
         const { data } = await axios.get(url,{
         headers: {
