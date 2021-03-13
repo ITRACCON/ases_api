a19d57630256681a21364382447aa0ee1ac7c4bd
ITRACCON <73070769+ITRACCON@users.noreply.github.com>
2021-03-13 21:46:42 +0300

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const app = express();
 const bodyParser = require('body-parser');
 require('./ases.js')();
 
+const Nightmare = require('nightmare');
 
+const nightmare = Nightmare({ show: true })
 // Use Node.js body parsing middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -31,35 +33,48 @@ app.get('/ases_api/upcoming_matches', (request, response) => {
     
 });
 
-app.get('/ases_api/live_matches', (request, response) => {
-    getLiveMatches().then(
+app.get('/ases_api/live_matches', async (request, response) => {
+   const url = "https://www.hltv.org/matches";
+    nightmare
+  .goto(url,{
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+         'User-Agent':	'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:86.0) Gecko/20100101 Firefox/86.0'
+        }},)
+  .wait('body')
+  .evaluate(() => document.querySelector('body').innerHTML)
+  .end()
+  .then(respon => {
+     getLiveMatches(respon).then(
     result => {
-      // первая функция-обработчик - запустится при вызове resolve
-  console.log("SUCCESS")
-      console.log(request);
-      response.json({"result": result});
+    
+   response.json({"result": result});
     },
     error => {
       // вторая функция - запустится при вызове reject
-      console.log("ERROR")
-      console.log(error)
+ 
       response.json({"ERROR": error});
     }
   );
+ 
+  }).catch(err => {
+    console.log(err);
+  });
+  console.log();
+
 });
 
 app.get('/ases_api/teams_raiting', (request, response) => {
     getTeamsRaiting().then(
     result => {
       // первая функция-обработчик - запустится при вызове resolve
-  console.log("SUCCESS")
-      console.log(request);
+
       response.json({"result": result});
     },
     error => {
       // вторая функция - запустится при вызове reject
-      console.log("ERROR")
-      console.log(error)
+
       response.json({"ERROR": error});
     }
   );
@@ -67,11 +82,11 @@ app.get('/ases_api/teams_raiting', (request, response) => {
 
 app.get('/ases_api/analitik', (request, response) => {
     console.log(request.body)
-  getMatchAnalitic(request.body.url).then(
+  getMatchAnalitic(' https://www.hltv.org/matches/2346705/pact-vs-sprout-esea-premier-season-36-europe').then(
   result => {
     // первая функция-обработчик - запустится при вызове resolve
 console.log("SUCCESS")
-    console.log(request);
+
     response.json({"result": result});
   },
   error => {
